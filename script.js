42ee63f28395a3e68c2dece0c9e0fbab6c85ef58
ashtonsprunger <sprungerashton@gmail.com>
2021-03-17 12:26:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// Time divs
+const mainTime = document.getElementById("main-time");
+const timeA = document.getElementById("time-a");
+const timeB = document.getElementById("time-b");
+// Stop buttons
+const stopA = document.getElementById("stop-a");
+const stopB = document.getElementById("stop-b");
+// Start button
+const startButton = document.getElementById("start");
+
+const minutes = document.querySelectorAll(".minutes");
+const seconds = document.querySelectorAll(".seconds");
+const tenths = document.querySelectorAll(".tenths");
+
+let START_TIME;
+
+let A_STOP = false;
+let B_STOP = false;
+
+let updateInterval;
+
+let RUNNING = false;
+
+function pad(num, size) {
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+}
+
+const updateTimes = () => {
+  mainTime.innerHTML = `${pad(
+    Math.floor(((Date.now() - START_TIME) % (1000 * 60 * 60)) / (1000 * 60)),
+    2
+  )}:${pad(
+    Math.floor(((Date.now() - START_TIME) % (1000 * 60)) / 1000),
+    2
+  )}.${Math.floor(((Date.now() - START_TIME) / 100) % 10)}`;
+  if (A_STOP && B_STOP) {
+    window.clearInterval(updateInterval);
+    RUNNING = false;
+    startButton.style.display = "block";
+    mainTime.innerHTML = "";
+  }
+};
+
+const startTimer = () => {
+  if (!RUNNING) {
+    A_STOP = false;
+    B_STOP = false;
+    timeA.innerHTML = "---------";
+    timeB.innerHTML = "---------";
+
+    RUNNING = true;
+    startButton.style.display = "none";
+    START_TIME = Date.now();
+    //   console.log(START_TIME);
+    updateInterval = setInterval(updateTimes, 100);
+  }
+};
+
+const firstStop = () => {
+  if (RUNNING && !A_STOP) {
+    A_STOP = true;
+    timeA.innerHTML = `${pad(
+      Math.floor(((Date.now() - START_TIME) % (1000 * 60 * 60)) / (1000 * 60)),
+      2
+    )}:${pad(
+      Math.floor(((Date.now() - START_TIME) % (1000 * 60)) / 1000),
+      2
+    )}.${Math.floor(((Date.now() - START_TIME) / 100) % 10)}`;
+  }
+};
+
+const secondStop = () => {
+  if (RUNNING && !B_STOP) {
+    B_STOP = true;
+    timeB.innerHTML = `${pad(
+      Math.floor(((Date.now() - START_TIME) % (1000 * 60 * 60)) / (1000 * 60)),
+      2
+    )}:${pad(
+      Math.floor(((Date.now() - START_TIME) % (1000 * 60)) / 1000),
+      2
+    )}.${Math.floor(((Date.now() - START_TIME) / 100) % 10)}`;
+  }
+};
+
+startButton.addEventListener("click", startTimer);
+
+stopA.addEventListener("touchstart", firstStop);
+stopB.addEventListener("touchstart", secondStop);
